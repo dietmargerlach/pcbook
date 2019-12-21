e4b705a27fe86afa9bc3c1e0a2b34b2b04d17c77
dietmargerlach <didi.gerlach@kabelmail.de>
2019-12-21 11:57:19 +0100

    added sample files

---
+++ sample/generator.go
@@ -0,0 +1,50 @@
+package sample
+
+import "github.com/dietmargerlach/pcbook/pb"
+
+// NewKeyboard should return a new sample keyboard ...
+func NewKeyboard() *pb.Keyboard {
+	keyboard := &pb.Keyboard{
+		Layout:  randomKeyboardLayout(),
+		Backlit: randomBool(),
+	}
+	return keyboard
+}
+
+// NewCPU ...
+func NewCPU() *pb.CPU {
+	brand := randomCPUBrand()
+	name := randomCPUName(brand)
+
+	numberCores := randomInt(2, 8)
+	numberThreads := randomInt(numberCores, 12)
+
+	minGhz := randomFloat64(2.0, 3.5)
+	maxGhz := randomFloat64(minGhz, 5.0)
+
+	cpu := &pb.CPU{
+		Brand:         brand,
+		Name:          name,
+		NumberCores:   uint32(numberCores),
+		NumberThreads: uint32(numberThreads),
+		MinGhz:        minGhz,
+		MaxGhz:        maxGhz,
+	}
+
+	return cpu
+}
+
+// NewGPU returns a new sample GPU object
+func NewGPU() *pb.GPU {
+	brand := randomGPUBrand()
+	name := randomGPUName(brand)
+
+	minGhz := randomFloat64(1.0, 1.5)
+	maxGhz := randomFloat64(minGhz, 2.0)
+
+	gpu := &pb.GPU{
+		Brand: brand,
+		Name:  name,
+	}
+	return gpu
+}
---
+++ sample/random.go
@@ -0,0 +1,82 @@
+package sample
+
+import (
+	"math/rand"
+
+	"github.com/dietmargerlach/pcbook/pb"
+)
+
+func randomKeyboardLayout() pb.Keyboard_Layout {
+	switch rand.Intn(3) {
+	case 1:
+		return pb.Keyboard_QWERTY
+	case 2:
+		return pb.Keyboard_QWERTZ
+	default:
+		return pb.Keyboard_AWERTY
+	}
+}
+
+func randomCPUBrand() string {
+	return randomStringFromSet("Intel", "AMD")
+}
+
+func randomCPUName(brand string) string {
+	if brand == "Intel" {
+		return randomStringFromSet(
+			"Xeon E",
+			"Core i9",
+			"Core i7",
+			"Core i5",
+			"Core i3",
+		)
+
+	}
+
+	return randomStringFromSet(
+		"Ryzen 7",
+		"Ryzen 5",
+		"Ryzen 3",
+	)
+}
+
+func randomGPUBrand() string {
+	return randomStringFromSet("NVIDIA", "AMD")
+}
+
+func randomGPUName(brand string) string {
+	if brand == "NVIDIA" {
+		return randomStringFromSet(
+			"RTX 2060",
+			"RTX 2070",
+			"GTX 1660",
+			"GTX 1070",
+		)
+	}
+	return randomStringFromSet(
+		"RX 590",
+		"RX 580",
+		"RX 5700",
+		"RX Vega 56",
+	)
+}
+
+func randomStringFromSet(a ...string) string {
+	n := len(a)
+	if n == 0 {
+		return ""
+	}
+	return a[rand.Intn(n)]
+}
+
+func randomBool() bool {
+	return rand.Intn(2) == 1
+}
+
+func randomInt(min int, max int) int {
+	return min + rand.Intn(max-min+1)
+}
+
+func randomFloat64(min float64, max float64) float64 {
+	return min + rand.Float64()*(max-min)
+}
